ffc298a16063daee100f87b2931f45b58be468e2
Victor <victor.touron@kbrwadventure.com>
2021-03-14 17:24:32 +0100

    chap2_init

--- mixproject/mix.exs
+++ mixproject/mix.exs
@@ -14,7 +14,7 @@ defmodule Mixproject.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :cowboy],
       mod: {Mixproject.Application, []}
     ]
   end
@@ -24,7 +24,9 @@ defmodule Mixproject.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:cowboy, "~> 1.1.2"},
+      {:plug, "~> 1.3.4"}
     ]
   end
 end
