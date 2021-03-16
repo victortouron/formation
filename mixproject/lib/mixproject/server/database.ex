fffd54ae9ef3984960bc710d6835ec4000857cbc
Victor <victor.touron@kbrwadventure.com>
2021-03-16 17:27:41 +0100

    corrections

--- mixproject/lib/mixproject/server/database.ex
+++ mixproject/lib/mixproject/server/database.ex
@@ -1,15 +1,16 @@
 defmodule Server.Database do
   use GenServer
   def create(database, {key, value}), do: GenServer.cast(Server.Database, {:create, database, {key, value}})
-  def read(database, key), do: GenServer.call(Server.Database, {database, key})
+  def read(database, key), do: GenServer.call(Server.Database, {:read, database, key})
   def update(database, {key, value}), do: GenServer.cast(Server.Database, {:update, database, {key, value}})
   def delete(database, key), do: GenServer.cast(Server.Database, {:delete, database, key})
+  def search(database, criteria), do: GenServer.call(Server.Database, {:search, database, criteria})
 
-  def handle_call({database, key}, _pid, intern_state), do: {:reply, :ets.lookup(database, key), intern_state}
   def handle_cast({:create, database, {key, value}}, intern_state) do
     :ets.insert_new(database, {key, value})
     {:noreply, intern_state}
   end
+  def handle_call({:read, database, key}, _pid, intern_state), do: {:reply, :ets.lookup(database, key), intern_state}
   def handle_cast({:update, database, {key, value}}, intern_state) do
     :ets.insert(database, {key, value})
     {:noreply, intern_state}
@@ -18,30 +19,23 @@ defmodule Server.Database do
     :ets.delete(database, key)
     {:noreply, intern_state}
   end
-
-
-  def search(database, criteria) do
+  def handle_call({:search, database, criteria}, _pid, intern_state) do
     list = :ets.tab2list(database)
     response = Enum.reduce(criteria, [], fn x, acc ->
-      id = elem(x, 0)
-      key = elem(x, 1)
+      {id, key} = x
       match = Enum.reduce(list, [], fn x, acc ->
-        order = Enum.reduce(Tuple.to_list(x), [], fn x, _acc ->
-          if is_map(x) == true do
-            if Map.get(x, id) == key, do: _acc = x
-          end
-        end)
-        if order != nil do
-            _acc = order
+        {_command, value} = x
+        {:ok, resp} = Map.fetch(value, id)
+        if resp == key do
+          _acc = x
         else
           acc
         end
       end)
       acc ++ [match]
     end)
-    {:ok, response}
+    {:reply, response, intern_state}
   end
-
 
   def start_link(initial_value) do
     IO.puts "Server Database Start Link"
--- mixproject/lib/mixproject/tuto_kbrw_stack.ex
+++ mixproject/lib/mixproject/tuto_kbrw_stack.ex
@@ -1,20 +1,20 @@
-db = :pokedex
-:ets.new(db, [:named_table, :public])
-
-Server.Supervisor.start_link(db)
-
-Server.Database.create(db, {:pokemon, "carapuce"})
-Server.Database.create(db, {:dresseur, "pierre"})
-IO.inspect Server.Database.read(db, :pokemon)
-IO.inspect Server.Database.read(db, :dresseur)
-Server.Database.update(db, {:pokemon, "bulbizarre"})
-Server.Database.update(db, {:dresseur, "sacha"})
-IO.inspect Server.Database.read(db, :pokemon)
-IO.inspect Server.Database.read(db, :dresseur)
-Server.Database.delete(db, :pokemon)
-Server.Database.delete(db, :dresseur)
-IO.inspect Server.Database.read(db, :pokemon)
-IO.inspect Server.Database.read(db, :dresseur)
+# db = :pokedex
+# :ets.new(db, [:named_table, :public])
+#
+Server.Supervisor.start_link(42)
+#
+# Server.Database.create(db, {:pokemon, "carapuce"})
+# Server.Database.create(db, {:dresseur, "pierre"})
+# IO.inspect Server.Database.read(db, :pokemon)
+# IO.inspect Server.Database.read(db, :dresseur)
+# Server.Database.update(db, {:pokemon, "bulbizarre"})
+# Server.Database.update(db, {:dresseur, "sacha"})
+# IO.inspect Server.Database.read(db, :pokemon)
+# IO.inspect Server.Database.read(db, :dresseur)
+# Server.Database.delete(db, :pokemon)
+# Server.Database.delete(db, :dresseur)
+# IO.inspect Server.Database.read(db, :pokemon)
+# IO.inspect Server.Database.read(db, :dresseur)
 
 db = :json
 :ets.new(db, [:named_table, :public])
@@ -22,5 +22,4 @@ IO.puts "Loading JSON into DB"
 JsonLoader.load_to_database(db, "/home/coachbombay/formation/mixproject/orders_dump/orders_chunk0.json")
 IO.puts "JSON Loaded Succes"
 
-{:ok, msg} = Server.Database.search(db, [{"order_number", "000147785"}, {"order_number", "000147784"}, {"order_number", "000147814"}])
-IO.inspect msg
+# IO.inspect Server.Database.search(db, [{"order_number", "000147785"}, {"order_number", "000147784"}])
