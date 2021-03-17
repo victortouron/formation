fa6b0cdeb7bc62f4bf97cbd0932933f250cdcf4f
Victor <victor.touron@kbrwadventure.com>
2021-03-17 18:35:55 +0100

    step0

--- mixproject/lib/mixproject/web/script.js
+++ mixproject/lib/mixproject/web/script.js
@@ -1,3 +1,9 @@
-function click() {
-  alert("test")
+var click = document.getElementById('click');
+click.onclick = () => {
+  const text = 'Hey I was created from JSX!';
+  const element = <div>{text}</div>;
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  );
 }
