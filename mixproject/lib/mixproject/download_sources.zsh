69cc296be7cbb41bdac98c7e4a6d1200807628f4
Victor <victor.touron@kbrwadventure.com>
2021-03-23 14:47:25 +0100

    step4

--- mixproject/lib/mixproject/download_sources.zsh
+++ mixproject/lib/mixproject/download_sources.zsh
@@ -1,7 +1,12 @@
 #!/bin/zsh
 
+# orders / order
 curl https://formation-kbrw.webflow.io/ > web/tuto.webflow/orders.html
-# curl https://uploads-ssl.webflow.com/6054744dba3801f99dab395f/css/formation-kbrw.webflow.fb1837f2a.css > web/tuto.webflow/css/tuto.webflow.css
-# wget -O - https://uploads-ssl.webflow.com/6054744dba3801f99dab395f/css/formation-kbrw.webflow.fb1837f2a.css | gunzip > web/tuto.webflow/css/tuto.webflow.css
 curl --compressed https://uploads-ssl.webflow.com/6054744dba3801f99dab395f/css/formation-kbrw.webflow.fb1837f2a.css > web/tuto.webflow/css/tuto.webflow.css
 curl https://formation-kbrw.webflow.io/order > web/tuto.webflow/order.html
+
+# modal / loader
+
+curl https://modal-9125ed.webflow.io/ > web/tuto.webflow/modal.html
+curl --compressed https://uploads-ssl.webflow.com/6059d3f8730f6145a529a455/css/modal-9125ed.webflow.1c7b2163e.css > web/tuto.webflow/css/modal.loader.css
+curl https://modal-9125ed.webflow.io/loader > web/tuto.webflow/loader.html
